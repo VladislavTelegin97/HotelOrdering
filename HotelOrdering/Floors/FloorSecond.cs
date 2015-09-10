ecbadde9e3fd3b5b035c1c6869f21a5d3cf4b6ab
Sergey Shulyak <sergshulya@gmail.com>
2015-09-11 01:44:48 +0300

    Floors moved to folder "Floors" and splitted in 3 files

---
+++ HotelOrdering/Floors/FloorSecond.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using HotelOrdering.Model;
+
+namespace HotelOrdering.Floors
+{
+    struct FloorSecond
+    {
+        Floor floor;
+
+        public Floor Floor
+        {
+            get { return floor; }
+            private set { floor = value; }
+        }
+
+        public FloorSecond (string kek = "kek")
+        {
+            #region FloorSecondInitialization
+
+            //Buttons:
+
+            Button f2r200 = new Button()
+            {
+                Size = new Size(49, 20),
+                Left = 8,
+                Top = 88,
+                BackColor = Color.White,
+                Cursor = Cursors.Hand,
+                Text = "200"
+            };
+
+            //Panels
+
+            Panel f2p200 = new Panel() { Size = new Size(45, 60), Left = 10, Top = 28, BackColor = Color.White };
+
+            //Floor creation
+
+            floor = new Floor(new List<Button>() {
+               f2r200
+           }, new List<Panel>() {
+               f2p200
+           });
+
+            #endregion
+        }
+    }
+}
---
+++ HotelOrdering/Floors/FloorThird.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using HotelOrdering.Model;
+
+namespace HotelOrdering.Floors
+{
+    struct FloorThird
+    {
+        Floor floor;
+
+        public Floor Floor
+        {
+            get { return floor; }
+            private set { floor = value; }
+        }
+
+        public FloorThird (string kek = "kek")
+        {
+            #region FloorThirdInitialization
+
+            //Buttons
+
+            Button f3r300 = new Button()
+            {
+                Size = new Size(49, 20),
+                Left = 118,
+                Top = 198,
+                BackColor = Color.White,
+                Cursor = Cursors.Hand,
+                Text = "300"
+            };
+
+            //Panels
+
+            Panel f3p300 = new Panel() { Size = new Size(45, 60), Left = 120, Top = 138, BackColor = Color.Green };
+
+            //Floor creating
+
+            floor = new Floor(new List<Button>() {
+               f3r300
+           }, new List<Panel>() {
+               f3p300
+           });
+
+            #endregion
+        }
+    }
+}
