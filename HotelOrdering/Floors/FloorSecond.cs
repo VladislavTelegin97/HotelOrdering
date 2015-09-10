2d2cd9a1349cdaf4633fd777182b89bd41bffaf2
Sergey Shulyak <sergshulya@gmail.com>
2015-09-11 01:59:16 +0300

    Added 1 room on th 2nd floor :)

--- HotelOrdering/Floors/FloorSecond.cs
+++ HotelOrdering/Floors/FloorSecond.cs
@@ -32,16 +32,26 @@ namespace HotelOrdering.Floors
                 Text = "200"
             };
 
+            Button f2r201 = new Button()
+            {
+                Size = new Size(49, 20),
+                Left = 8,
+                Top = 185,
+                BackColor = Color.White,
+                Cursor = Cursors.Hand,
+                Text = "201"
+            };
+
             //Panels
 
             Panel f2p200 = new Panel() { Size = new Size(45, 60), Left = 10, Top = 28, BackColor = Color.White };
-
+            Panel f2p201 = new Panel() { Size = new Size(45, 60), Left = 10, Top = 125, BackColor = Color.White };
             //Floor creation
 
             floor = new Floor(new List<Button>() {
-               f2r200
+               f2r200, f2r201
            }, new List<Panel>() {
-               f2p200
+               f2p200, f2p201
            });
 
             #endregion
